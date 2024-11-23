8c625396bef6d2f975cb3b1ddc52a2145d0b8841
Andras E <rohadtsajt@gmail.com>
2024-11-23 10:08:54 +0000

    null vs undifined

---
+++ fundamentals/03_null_undifinded.js
@@ -0,0 +1,10 @@
+var title;
+console.log("title " + title);
+if (!title) {
+    console.log("The value of title is not known yet.");
+}
+var subTitle = null;
+console.log("title " + subTitle);
+if (!subTitle) {
+    console.log("The value of subTitle is not known yet.");
+}
---
+++ fundamentals/03_null_undifinded.ts
@@ -0,0 +1,15 @@
+let title:string;
+
+console.log("title " + title);
+
+if (!title) {
+    console.log("The value of title is not known yet.")
+}
+
+let subTitle:string = null;
+
+console.log("title " + subTitle);
+
+if (!subTitle) {
+    console.log("The value of subTitle is not known yet.")
+}
