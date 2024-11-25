4bf4714082eadf64ed7fd6c9371be1a68ec4f74b
Andras E <rohadtsajt@gmail.com>
2024-11-25 19:09:34 +0000

    arrays & enums

---
+++ fundamentals/04_arrays.ts
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3]
+
+numbers.push(4)
+
+// would`t work Ts flag it as its number, but nothing special about TS arrrays
+// numbers.push("asd")
---
+++ fundamentals/05_enums.ts
@@ -0,0 +1,12 @@
+enum courseType {
+    FREE=5,
+    PREMIUM =4123 ,
+    PRIVATE = "hello"
+}
+
+const course = {
+    title: 'Ts Course',
+    type: courseType.PREMIUM,
+}
+
+console.log(course)
