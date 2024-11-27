52ad0148f5c0c841899ce8dab7ed41e45abb0330
Andras E <rohadtsajt@gmail.com>
2024-11-27 17:08:40 +0000

    any, noImplicitAny

---
+++ fundamentals/06_any_type.js
@@ -0,0 +1,6 @@
+var lessionsCount = 10;
+lessionsCount = "Some string";
+var number = [10, "anything", true];
+function printCourse(title, lessionsCount) {
+    console.log("title = ".concat(title, ", lessionsCount = ").concat(lessionsCount));
+}
---
+++ fundamentals/06_any_type.ts
@@ -0,0 +1,9 @@
+let lessionsCount:any = 10;
+
+lessionsCount = "Some string"
+
+ let number:any [] = [10, "anything", true]
+
+function  printCourse(title, lessionsCount) {
+    console.log(`title = ${title}, lessionsCount = ${lessionsCount}`);
+}
