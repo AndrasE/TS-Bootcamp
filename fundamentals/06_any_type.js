b928247d3d2bc03b3e20cd52acd02e4897443419
Andras E <rohadtsajt@gmail.com>
2024-11-27 17:31:53 +0000

    non null assertion operator

--- fundamentals/06_any_type.js
+++
@@ -1,6 +0,0 @@
-var lessionsCount = 10;
-lessionsCount = "Some string";
-var number = [10, "anything", true];
-function printCourse(title, lessionsCount) {
-    console.log("title = ".concat(title, ", lessionsCount = ").concat(lessionsCount));
-}
