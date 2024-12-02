3b84868e891659265907138a6844adb3b6dfe7cf
Andras E <rohadtsajt@gmail.com>
2024-12-02 17:05:18 +0000

    module import/exports/re-exports

---
+++ fundamentals/12_modules_inport.ts
@@ -0,0 +1,9 @@
+import {PAGE_SIZE, COURSE, Course} from "./13_modules_exports";
+
+const pageSize = PAGE_SIZE;
+
+const course = COURSE
+
+export {
+    PAGE_SIZE as MAX_PAGE_SIZE,
+}
---
+++ fundamentals/13_modules_exports.ts
@@ -0,0 +1,13 @@
+export type Course = {
+    readonly title: string,
+    description: string,
+    lessonCount?: number,
+}
+
+export const PAGE_SIZE = 100
+
+export const COURSE = {
+    title: "TS",
+    description: "Bootcamp",
+    lessonCount: 10
+}
