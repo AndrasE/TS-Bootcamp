8d14d46b26b62c32576908898b4007d3ac0bdb7f
Andras E <rohadtsajt@gmail.com>
2025-01-08 18:37:45 +0000

    never type

---
+++ fundamentals/27_never_type.ts
@@ -0,0 +1,11 @@
+type CourseStatus = "draft" | "published";
+
+let courseStatus: CourseStatus;
+
+if (courseStatus == "published") {
+    console.log("published")
+} else if (courseStatus == "draft") {
+    console.log("draft")
+} else {
+    const value = courseStatus
+}
