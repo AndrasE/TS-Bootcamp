977cb1bc0d644bcb3907f6878646cf123e9976e4
AndrasE <rohadtsajt@gmail.com>
2025-01-21 17:25:57 +0000

    classes constructor member variables etc

--- classes/01_classes_intro.ts
+++ classes/01_classes_intro.ts
@@ -1,25 +1,40 @@
 class Course {
-    title : string;
-    subtitle : string;
-    creationDate : Date;
+  title: string;
+  subtitle: string;
+  creationDate: Date;
 
-    constructor(
-        title: string,
-        subtitle: string,
-        creationDate : Date
-    ) {
-        this.title = title;
-        this.subtitle = subtitle;
-        this.creationDate = creationDate;
-    }
+  constructor(title: string, subtitle: string, creationDate: Date) {
+    this.title = title;
+    this.subtitle = subtitle;
+    this.creationDate = creationDate;
+  }
 
-    age() {
-      const ageInMS =  new Date().getTime() - this.creationDate.getTime();
+  age() {
+    const ageInMS = new Date().getTime() - this.creationDate.getTime();
 
-      return ageInMS / 1000 / 60 / 24;
-    }
+    return ageInMS / 1000 / 60 / 24;
+  }
 }
 
-const course = new Course("TS", "Bootcamp", new Date(2001,1,22))
+const course = new Course("TS", "Bootcamp", new Date(2001, 1, 22));
 
-console.log(course.age())
+console.log(course.age());
+
+//symplified syntax
+
+class Course2 {
+  constructor(
+    public title: string,
+    private subtitle: string,
+    private creationDate: Date
+  ) {}
+  age() {
+    const ageInMS = new Date().getTime() - this.creationDate.getTime();
+
+    return ageInMS / 1000 / 60 / 24;
+  }
+}
+
+const course2 = new Course2("TS", "Bootcamp", new Date(2001, 1, 22));
+
+console.log(course2.age());
