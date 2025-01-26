e2aec4278e2ac7a3ad16443acda9474e59d8935b
AndrasE <rohadtsajt@gmail.com>
2025-01-26 10:48:00 +0000

    singleton

---
+++ classes/singletron.ts
@@ -0,0 +1,13 @@
+export class CourseService {
+  private static INSTANCE: CourseService;
+
+  private constructor() {
+    console.log("CourseService initialized");
+  }
+  static instance() {
+    if (!CourseService.INSTANCE) {
+      CourseService.INSTANCE = new CourseService();
+    }
+    return CourseService.INSTANCE;
+  }
+}
