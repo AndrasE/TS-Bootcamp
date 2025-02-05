bdf49bb33d00dd1b550b993246fdfebea9c2f03b
Andras E <rohadtsajt@gmail.com>
2025-02-05 19:17:15 +0000

    server running
    -created routes folder and root.ts
    -set up endpoints
    -updated to ESNext so have import express from "express"; import * as express from 'express';
    target": "ESNext", "esModuleInterop": true, "moduleResolution": "nodenext", "module": "NodeNext"

--- rest-api/dist/server.js
+++ rest-api/dist/server.js
@@ -1,1 +1,19 @@
-console.log("hellooo world");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const root_1 = require("./routes/root");
+const app = (0, express_1.default)();
+function setupExpress() {
+    // http://localhost:3000/
+    app.route("/").get(root_1.root);
+}
+function startServer() {
+    app.listen(3000, () => {
+        console.log("Server started on port 3000");
+    });
+}
+setupExpress();
+startServer();
--- rest-api/src/server.ts
+++ rest-api/src/server.ts
@@ -1,13 +1,20 @@
-import * as express from 'express';
+import express from "express";
+import {root} from "./routes/root";
+
+const app  = express();
 
 function setupExpress() {
-
+    // http://localhost:3000/
+    app.route("/").get(root);
 }
 
 function startServer() {
-
+    app.listen(3000, ()=> {
+        console.log("Server started on port 3000");
+    });
 }
 
 setupExpress();
 
 startServer();
+
