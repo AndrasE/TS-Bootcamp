a6512d56012f73a5415296e2406e3bd3d7cdbd95
AndrasE <rohadtsajt@gmail.com>
2025-02-06 19:08:59 +0000

    clean dist folder hot reload

--- rest-api/dist/routes/root.js
+++ rest-api/dist/routes/root.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.root = root;
 function root(request, response) {
-    response.status(200).send("<h1>Hello World Express!</h1>");
+    response.status(200).send("<h1>Server is running via Express!</h1>");
 }
--- rest-api/src/routes/root.ts
+++ rest-api/src/routes/root.ts
@@ -1,5 +1,5 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 
 export function root(request: Request, response: Response) {
-    response.status(200).send("<h1>Hello World Express!</h1>");
+  response.status(200).send("<h1>Server is running via Express!</h1>");
 }
