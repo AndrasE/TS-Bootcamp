2893a874a3e1d114a44da50798b80b70c5711967
Andras E <rohadtsajt@gmail.com>
2025-03-14 06:39:14 +0000

    error handling using NextFunction from express

--- rest-api/dist/routes/get-all-courses.js
+++ rest-api/dist/routes/get-all-courses.js
@@ -3,12 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.gettAllCourses = gettAllCourses;
 const logger_1 = require("../logger");
 const data_source_1 = require("../data-source");
-async function gettAllCourses(request, response) {
+async function gettAllCourses(request, response, next) {
     logger_1.logger.info("Getting all courses");
-    const courses = await data_source_1.AppDataSource.getRepository("Course")
-        .createQueryBuilder("courses")
-        .leftJoinAndSelect("courses.lessons", "LESSONS")
-        .orderBy("courses.seqNo")
-        .getMany();
-    response.status(200).json({ courses });
+    try {
+        const courses = await data_source_1.AppDataSource.getRepository("Course")
+            .createQueryBuilder("courses")
+            .orderBy("courses.seqNo")
+            .getMany();
+        response.status(200).json({ courses });
+    }
+    catch (error) {
+        logger_1.logger.error("Error getting all courses", error);
+        return next(error);
+    }
 }
--- rest-api/src/routes/get-all-courses.ts
+++ rest-api/src/routes/get-all-courses.ts
@@ -1,15 +1,23 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { logger } from "../logger";
 import { AppDataSource } from "../data-source";
 
-export async function gettAllCourses(request: Request, response: Response) {
+export async function gettAllCourses(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
   logger.info("Getting all courses");
 
-  const courses = await AppDataSource.getRepository("Course")
-    .createQueryBuilder("courses")
-    .leftJoinAndSelect("courses.lessons", "LESSONS")
-    .orderBy("courses.seqNo")
-    .getMany();
+  try {
+    const courses = await AppDataSource.getRepository("Course")
+      .createQueryBuilder("courses")
+      .orderBy("courses.seqNo")
+      .getMany();
 
-  response.status(200).json({ courses });
+    response.status(200).json({ courses });
+  } catch (error) {
+    logger.error("Error getting all courses", error);
+    return next(error);
+  }
 }
