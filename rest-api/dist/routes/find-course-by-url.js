f3fe576c4d52ff362ba731a89e198e7a820fe6ac
Andras E <rohadtsajt@gmail.com>
2025-03-18 18:57:22 +0000

    Filtering Query results TypeORM (new data retrieval endpoints) 2

--- rest-api/dist/routes/find-course-by-url.js
+++ rest-api/dist/routes/find-course-by-url.js
@@ -2,14 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.findCourseByUrl = findCourseByUrl;
 const logger_1 = require("../logger");
+const data_source_1 = require("../data-source");
+const lesson_1 = require("../models/lesson");
 async function findCourseByUrl(request, response, next) {
-    logger_1.logger.info("Getting all courses");
-    const courseUrl = request.params.courseUrl;
-    if (!courseUrl) {
-        throw "Missing courseUrl parameter";
-    }
     try {
         logger_1.logger.debug("Called findCourseByUrl()");
+        const courseUrl = request.params.courseUrl;
+        if (!courseUrl) {
+            throw "Missing courseUrl parameter";
+        }
+        const course = await data_source_1.AppDataSource.getRepository("Course").findOneBy({
+            url: courseUrl,
+        });
+        if (!course) {
+            const message = `Could not find course with url ${courseUrl}`;
+            logger_1.logger.error(message);
+            response.status(404).json({ message });
+            return;
+        }
+        const totalLessons = await data_source_1.AppDataSource.getRepository(lesson_1.Lesson)
+            .createQueryBuilder("lessons")
+            .where("lessons.courseId = :courseId", { courseId: course.id })
+            .getCount();
+        response.status(200).json({ course, totalLessons });
     }
     catch (error) {
         logger_1.logger.error("Error calling findCourseByUrl", error);
--- rest-api/src/routes/find-course-by-url.ts
+++ rest-api/src/routes/find-course-by-url.ts
@@ -1,21 +1,39 @@
 import { Request, Response, NextFunction } from "express";
 import { logger } from "../logger";
+import { AppDataSource } from "../data-source";
+import { Lesson } from "../models/lesson";
 
 export async function findCourseByUrl(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
-  logger.info("Getting all courses");
-
-  const courseUrl = request.params.courseUrl;
-
-  if (!courseUrl) {
-    throw "Missing courseUrl parameter";
-  }
-
   try {
     logger.debug("Called findCourseByUrl()");
+
+    const courseUrl = request.params.courseUrl;
+
+    if (!courseUrl) {
+      throw "Missing courseUrl parameter";
+    }
+
+    const course = await AppDataSource.getRepository("Course").findOneBy({
+      url: courseUrl,
+    });
+
+    if (!course) {
+      const message = `Could not find course with url ${courseUrl}`;
+      logger.error(message);
+      response.status(404).json({ message });
+      return;
+    }
+
+    const totalLessons = await AppDataSource.getRepository(Lesson)
+      .createQueryBuilder("lessons")
+      .where("lessons.courseId = :courseId", { courseId: course.id })
+      .getCount();
+
+    response.status(200).json({ course, totalLessons });
   } catch (error) {
     logger.error("Error calling findCourseByUrl", error);
     return next(error);
