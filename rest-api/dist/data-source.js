7c4c1eeaaa1ccaa5d858a3b7a386f76b4fafedf8
Andras E <rohadtsajt@gmail.com>
2025-03-27 12:35:51 +0000

    securin rest api start - Adding users entity our modal and include in the data source

--- rest-api/dist/data-source.js
+++ rest-api/dist/data-source.js
@@ -4,6 +4,7 @@ exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 const course_1 = require("./models/course");
 const lesson_1 = require("./models/lesson");
+const user_1 = require("./models/user");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
     host: process.env.DB_HOST,
@@ -12,7 +13,7 @@ exports.AppDataSource = new typeorm_1.DataSource({
     port: parseInt(process.env.DB_PORT),
     database: process.env.DB_NAME,
     ssl: true,
-    entities: [course_1.Course, lesson_1.Lesson],
+    entities: [course_1.Course, lesson_1.Lesson, user_1.User],
     synchronize: true,
     logging: true,
 });
